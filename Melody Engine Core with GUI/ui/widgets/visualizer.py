afba38caf3e5bde3273ee6910f8b65f5b91e5c99
Khritish Kumar Behera <khritish.official@gmail.com>
2026-07-22 17:31:36 +0530

    visualizer and audio player ui added

--- Melody Engine Core with GUI/ui/widgets/visualizer.py
+++ Melody Engine Core with GUI/ui/widgets/visualizer.py
@@ -1,5 +1,6 @@
 import flet as ft
 from ui import theme
+import random
 
 def visualizer_panel():
     return ft.Container(
@@ -90,4 +91,83 @@ def audio_player():
         expand=True,
         bgcolor="#0E162A",
         border_radius=10,
+        padding=15,
+        content=audio_panel(),
     )
+
+def audio_panel():
+    return ft.Column(
+        controls=[
+            visualizer(),
+
+            audio_time_line_tray(),
+            audio_buttons_tray(),
+        ]
+    )
+
+
+
+play_button = ft.IconButton(icon=ft.Icons.PLAY_ARROW, icon_color="white", bgcolor="#5D64F8", icon_size=20,)
+
+def audio_buttons_tray():
+    return ft.Container(
+        content=ft.Row(
+            alignment=ft.MainAxisAlignment.START,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                ft.Container(width=10),
+                play_button,
+                ft.Container(width=20),
+                ft.Icon(icon=ft.Icons.VOLUME_UP),
+                ft.Container(padding=0, content=ft.Slider(value = 60, min=0, max = 100, divisions=100, active_color="#5D64F8",width=150),),
+            ]
+        )
+    )
+
+total_time = "01:36"
+ellapsed_time = "00:55"
+def audio_time_line_tray():
+    t_time = total_time.split(":")
+    e_time = ellapsed_time.split(":")
+    total_time_sec = (int(t_time[0]) * 60) + int(t_time[1])
+    ellapsed_time_sec = (int(e_time[0]) * 60) + int(e_time[1])
+    return ft.Container(
+        content=ft.Column(
+            spacing=0,
+            controls=[
+                ft.Container(padding=0, height=20, 
+                             content=ft.Slider(expand=True, value = ellapsed_time_sec, min=0, max = total_time_sec, divisions=1000, active_color="#5D64F8"),
+                             ),
+                ft.Container(padding=ft.Padding.symmetric(horizontal=15),content=ft.Row(
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                    controls=[
+                        ft.Text(ellapsed_time),
+                        ft.Text(total_time)]
+                )),
+                
+            ],
+        )
+    )
+
+bars = []
+colors = [
+    "#EC2F7F", "#F13388", "#F53896", "#F73FA5",
+    "#F245B5", "#F15DF1", "#E96AF8", "#D96BFC",
+    "#C96EFF", "#BD6CFF", "#A876FF", "#9370F8",
+    "#7E73F3", "#677CF5", "#5887FA", "#4993FD",
+    "#339EFE", "#1EA8F5", "#0BAAEB", "#08BCEB",
+    "#07D5F3", "#07FAF8", "#19E8DD", "#34D8CE",
+    "#53D5A7", "#69EF5D", "#8AF158", "#A8F354",
+    "#C5F354", "#DFED47", "#F4E73C", "#F6C934",
+    "#F5A22C", "#FF8531", "#FF673D", "#F23E5D"
+]
+for i in range(len(colors)):
+    bars.append(ft.Container(expand=True, height=random.randint(50, 250), border_radius=5, bgcolor=colors[i]))
+def visualizer():
+    return ft.Container(expand = True,
+                        content=ft.Row(
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            vertical_alignment=ft.CrossAxisAlignment.END,
+                            spacing=3,
+                            controls=bars,)
+                            )
